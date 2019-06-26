1cd24ef1eac7c345c312198ad99a7d1540716dc4
Heidi Engstrom <hengstrom25@gmail.com>
2019-06-26 20:56:38 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react', 
 
 class Comment extends Component {
-  
+  constructor(props)
+    super(props)
+    
+    render() {
+      
+      return
+    }
   
   
   
