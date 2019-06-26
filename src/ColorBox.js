744a6e286019b8f6da66962bef9b1bc23d40281c
Heidi Engstrom <hengstrom25@gmail.com>
2019-06-26 21:03:59 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,10 +4,17 @@ class ColorBox extends Component {
   
   
   render() {
-    return (
-      
+		const opac = this.props.opacity
+		
+		if (opac >= 0.2) {
+		return (
+        	<div className="color-box" style={{opacity: opac}}>
+          		<ColorBox opacity={opac - .1}/>
+        	</div>
       )
-  }
+      } else {
+      	return null;
+      }
   
 }
 
